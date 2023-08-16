3983a99f87b1ce96186f5b3c5621cdfa3ac47a58
Sima Gelfman <gelfmansima@gmail.com>
2023-08-17 00:45:53 +0700

    Add delete group test Add general fixture by adding conftest

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,42 +1,22 @@
 # -*- coding: utf-8 -*-
-from fixture.application import Application
+
 from model.Contact import Contact
-import pytest
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_new_contact(app):
-
     app.session.login(login="admin", password="secret")
 
     app.contact.create_new(Contact("Boris", "Vlad", "Groen", "Boris",
-                                        "Director", "Compony_1", "Mosc", "888888", "99999",
-                                        "99999", "00000", "bls"))
+                                   "Director", "Compony_1", "Mosc", "888888", "99999",
+                                   "99999", "00000", "bls"))
 
     app.session.logout()
 
+
 def test_add_empty_contact(app):
+    app.session.login(login="admin", password="secret")
 
-   app.session.login(login="admin", password="secret")
+    app.contact.create_new(Contact("", "", "", "", "", "", "", "", "",
+                                   "", "", ""))
 
-   app.contact.create_new(Contact("", "", "", "", "", "", "", "", "",
-                                        "", "", ""))
-
-   app.session.logout()
-
-
-
-
-
-
-
-
-
-
-
+    app.session.logout()
